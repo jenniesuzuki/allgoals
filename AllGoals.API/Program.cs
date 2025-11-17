bb61ec7bb7be2f612d76f955f145dc2dabbd7bba
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2025-11-17 01:21:53 -0300

    feat: add health checks, logging and tracing

--- AllGoals.API/Program.cs
+++ AllGoals.API/Program.cs
@@ -3,43 +3,94 @@ using AllGoals.Application.Services;
 using AllGoals.Domain.Interfaces;
 using AllGoals.Infrastructure.Data;
 using AllGoals.Infrastructure.Repositories;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
-using Oracle.EntityFrameworkCore.Infrastructure;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog; 
 
-var builder = WebApplication.CreateBuilder(args);
-        
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseOracle(connectionString, oraOptions => 
-    {
-
-    })
-);
-
-builder.Services.AddScoped<IGoalRepository, GoalRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IStoreItemRepository, StoreItemRepository>();
-
-builder.Services.AddScoped<IGoalService, GoalService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IStoreItemService, StoreItemService>();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Iniciando a API AllGoals...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddOpenTelemetry()
+        .WithTracing(tracerProviderBuilder =>
+        {
+            tracerProviderBuilder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("AllGoals.API"))
+                .AddAspNetCoreInstrumentation()
+                .AddEntityFrameworkCoreInstrumentation()
+                .AddConsoleExporter();
+        });
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    builder.Services.AddHealthChecks()
+        .AddCheck("self", () => Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckResult.Healthy())
+        .AddOracle(connectionString!, name: "oracle-db", tags: new[] { "db", "data" });
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseOracle(connectionString, oraOptions => 
+        {
+
+        })
+    );
+
+    builder.Services.AddScoped<IGoalRepository, GoalRepository>();
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IStoreItemRepository, StoreItemRepository>();
+
+    builder.Services.AddScoped<IGoalService, GoalService>();
+    builder.Services.AddScoped<IUserService, UserService>();
+    builder.Services.AddScoped<IStoreItemService, StoreItemService>();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapHealthChecks("/health", new HealthCheckOptions
+    {
+        Predicate = _ => true
+    });
+
+    app.MapHealthChecks("/health/details", new HealthCheckOptions
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-        
-app.UseAuthorization();
-
-app.MapControllers();
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "A aplicação falhou ao iniciar inesperadamente");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
